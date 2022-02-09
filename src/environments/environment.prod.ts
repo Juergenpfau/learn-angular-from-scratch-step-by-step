65b9beac3660db1c393c7d5b35cd257162ed74a1
Juergenpfau <60987304+Juergenpfau@users.noreply.github.com>
2022-02-08 19:41:03 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://b4jwwkbri5.execute-api.us-east-1.amazonaws.com/api'
+  api_url: 'https://b4jwwkbri5.execute-api.us-east-1.amazonaws.com'
 };
